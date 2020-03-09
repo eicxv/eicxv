774429c199d4df4f35549ffc83d120763e06e10f
Einar Persson <einar.jpg@gmail.com>
2020-03-09 01:01:26 +0100

    cleaned up shader code

--- src/materials/reflectMaterial.js
+++ src/materials/reflectMaterial.js
@@ -1,52 +1,35 @@
-import { UniformsLib } from "three/src/renderers/shaders/UniformsLib";
-import { mergeUniforms } from "three/src/renderers/shaders/UniformsUtils";
-
 const vertexShader = /* glsl */ `
-
-varying vec3 vViewPosition;
+varying vec3 vertexPosition;
 
 void main() {
 
+	vec4 mvPosition = vec4( position, 1.0 );
+  mvPosition = modelViewMatrix * mvPosition;
+  gl_Position = projectionMatrix * mvPosition;
 
-	vec3 transformed = vec3( position );
-	vec4 mvPosition = vec4( transformed, 1.0 );
-    mvPosition = modelViewMatrix * mvPosition;
-    gl_Position = projectionMatrix * mvPosition;
-    
-
-
-	vViewPosition = transformed;
+	vertexPosition = position;
 }
 `;
 
 const fragmentShader = /* glsl */ `
 uniform float opacity;
-// uniform vec3 lightDir;
 
-varying vec3 vViewPosition;
-
-vec3 packNormalToRGB( const in vec3 normal ) {
-	return normalize( normal ) * 0.5 + 0.5;
-}
+varying vec3 vertexPosition;
 
 void main() {
 
-	vec3 fdx = vec3( dFdx( vViewPosition.x ), dFdx( vViewPosition.y ), dFdx( vViewPosition.z ) );
-	vec3 fdy = vec3( dFdy( vViewPosition.x ), dFdy( vViewPosition.y ), dFdy( vViewPosition.z ) );
-    vec3 normal = normalize( cross( fdx, fdy ) );
+	vec3 fdx = dFdx( vertexPosition );
+	vec3 fdy = dFdy( vertexPosition );
+  vec3 normal = normalize( cross( fdx, fdy ) );
 
-    vec3 cameraDir = vViewPosition - cameraPosition;
-    vec3 reflected = normalize(cameraDir - (2. * dot(cameraDir, normal) * normal));
+  vec3 cameraDir = vertexPosition - cameraPosition;
+  vec3 reflected = normalize(cameraDir - (2. * dot(cameraDir, normal) * normal));
     
-    vec3 lightDir = normalize( - vec3(1, 0, -1));
-    float align = dot(lightDir, reflected);
-    float val = floor(align + 0.005);
-    // float val = align * 80. - 79.5;
+  vec3 lightDir = normalize( - vec3(1, 0, -1));
+  float align = dot(lightDir, reflected);
+  float val = floor(align + 0.005);
 
-    gl_FragColor = vec4( val, val, val, opacity);
-
-	// gl_FragColor = vec4( packNormalToRGB( normal ), opacity );
-
+  gl_FragColor = vec4( val, val, val, opacity);
 }
 `;
 
