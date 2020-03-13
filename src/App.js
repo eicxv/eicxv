dce22a3e76129edb6a49324b5e96f54bc3ca0577
Einar Persson <einar.jpg@gmail.com>
2020-03-13 20:08:19 +0100

    Added theme and theme provider
    Created Home.js with App.js content, added theme and theme provider to App.js

--- src/App.js
+++ src/App.js
@@ -1,76 +1,20 @@
-import React, { useState } from "react";
+import React from "react";
 
 // material ui
-import { Typography } from "@material-ui/core/";
-import { makeStyles } from "@material-ui/core/styles";
+import { ThemeProvider, createMuiTheme } from "@material-ui/core/styles";
+import purple from "@material-ui/core/colors/purple";
+import green from "@material-ui/core/colors/green";
 
 // custom components
-import Header from "./components/Header";
-import About from "./components/About";
-import DownButton from "./components/DownButton";
-import Post from "./components/Post";
-import PostPreview from "./components/PostPreview";
+import Home from "./components/Home";
 
-import ReflectionSketch from "./components/ReflectionSketch";
-
-//dummy data
-import posts from "./dummyPosts";
-
-const useStyles = makeStyles({
-  background: {
-    backgroundColor: "black",
-    scrollBehavior: "smooth"
-  },
-  introText: {
-    position: "absolute",
-    left: "90px",
-    top: "70px",
-    color: "white",
-    fontFamily: "'Archivo', serif",
-    fontSize: "4rem",
-    pointerEvents: "none"
-  },
-  sketch: { width: "100%", height: "100vh", display: "block" },
-  downButtonContainer: {
-    display: "flex",
-    justifyContent: "center",
-    width: "100%",
-    position: "absolute",
-    bottom: "50px"
-  }
-});
+const theme = createMuiTheme({});
 
 function App() {
-  const classes = useStyles();
-  const [aboutOpen, setAboutOpen] = useState();
-
-  function toggleAbout() {
-    setAboutOpen(!aboutOpen);
-  }
-
-  function scrollToLatest() {
-    let headerHeight = document.getElementById("header").offsetHeight;
-    window.scrollTo({
-      top: window.innerHeight - headerHeight,
-      behavior: "smooth"
-    });
-  }
-
   return (
-    <div className={classes.background}>
-      <Header onAbout={toggleAbout} />
-      <About open={aboutOpen} onClose={toggleAbout} />
-      <Typography className={classes.introText} align="left">
-        Lorem Ipsum
-      </Typography>
-      <ReflectionSketch canvasStyle={classes.sketch} />
-      <div className={classes.downButtonContainer}>
-        <DownButton onClick={scrollToLatest} />
-      </div>
-      {posts.map(post => (
-        <PostPreview post={post} key={post.meta.id} />
-      ))}
-    </div>
+    <ThemeProvider theme={theme}>
+      <Home />
+    </ThemeProvider>
   );
 }
 
---
+++ src/components/Home.js
@@ -0,0 +1,80 @@
+import React, { useState } from "react";
+
+// material ui
+import { Typography } from "@material-ui/core/";
+import { makeStyles, useTheme } from "@material-ui/core/styles";
+
+// custom components
+import Header from "./Header";
+import About from "./About";
+import DownButton from "./DownButton";
+import Post from "./Post";
+import PostPreview from "./PostPreview";
+
+import ReflectionSketch from "./ReflectionSketch";
+
+//dummy data
+import posts from "../dummyPosts";
+
+const useStyles = makeStyles({
+  background: {
+    backgroundColor: "black",
+    scrollBehavior: "smooth"
+  },
+  introText: {
+    position: "absolute",
+    left: "90px",
+    top: "70px",
+    color: "white",
+    fontFamily: "'Archivo', serif",
+    fontSize: "4rem",
+    pointerEvents: "none"
+  },
+  sketch: { width: "100%", height: "100vh", display: "block" },
+  downButtonContainer: {
+    display: "flex",
+    justifyContent: "center",
+    width: "100%",
+    position: "absolute",
+    bottom: "50px"
+  }
+});
+
+function Home() {
+  const theme = useTheme();
+  console.log(theme);
+
+  const classes = useStyles();
+  const [aboutOpen, setAboutOpen] = useState();
+
+  function toggleAbout() {
+    setAboutOpen(!aboutOpen);
+  }
+
+  function scrollToLatest() {
+    let headerHeight = document.getElementById("header").offsetHeight;
+    window.scrollTo({
+      top: window.innerHeight - headerHeight,
+      behavior: "smooth"
+    });
+  }
+
+  return (
+    <div className={classes.background}>
+      <Header onAbout={toggleAbout} />
+      <About open={aboutOpen} onClose={toggleAbout} />
+      <Typography className={classes.introText} align="left">
+        Lorem Ipsum
+      </Typography>
+      <ReflectionSketch canvasStyle={classes.sketch} />
+      <div className={classes.downButtonContainer}>
+        <DownButton onClick={scrollToLatest} />
+      </div>
+      {posts.map(post => (
+        <PostPreview post={post} key={post.meta.id} />
+      ))}
+    </div>
+  );
+}
+
+export default Home;
