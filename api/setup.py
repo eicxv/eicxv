6935103aa0a757ea0662e6fcbed14264bf040e78
Einar Persson <einar.jpg@gmail.com>
2020-03-28 14:50:45 +0100

    updated setup.py to find packages automatically
    fixed database_github_sync not included in distribution

--- api/setup.py
+++ api/setup.py
@@ -1,9 +1,9 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(
     name="eicxv_api",
     version="0.0.1",
-    packages=["eicxv_api",],
+    packages=find_packages(),
     license="GNU GENERAL PUBLIC LICENSE v3",
     python_requires=">=3.7",
     install_requires=[
