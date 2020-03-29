d186a686f81225c2710c9f47a046f7404506052a
Einar Persson <einar.jpg@gmail.com>
2020-03-29 11:21:01 +0200

    redesigned postpreview cards

--- frontend/src/components/PostPreview.js
+++ frontend/src/components/PostPreview.js
@@ -1,71 +1,95 @@
 import React from "react";
 
 // react router
-import { useHistory } from "react-router";
+import { Link } from "react-router-dom";
 
 // material ui
-import { Typography } from "@material-ui/core";
+import { Typography, Hidden, ButtonBase } from "@material-ui/core";
 import { makeStyles } from "@material-ui/core/styles";
 
-// custom components
-import Container from "./Container";
-
-const useStyles = makeStyles({
-  container: {
-    display: "flex",
-    alignItems: "center"
-  },
-  title: {
-    fontFamily: "'Archivo', sans-serif",
-    fontSize: "2rem"
-  },
-  introText: {
-    maxHeight: "14rem",
-    fontFamily: "'Montserrat', sans-serif",
-    fontSize: "1rem",
-    textOverflow: "ellipsis"
-  },
-  metaData: {
-    fontFamily: "'Space Mono', sans-serif",
-    fontSize: "0.625rem"
-  },
-  text: {
-    minWidth: "50%",
-    flexGrow: "1",
-    flexBasis: "0"
-  },
-  image: {
-    display: "block",
-    width: "auto",
-    height: "auto",
-    maxWidth: "100%",
-    maxHeight: "14rem",
-    marginLeft: "3rem"
-  }
+const useStyles = makeStyles(theme => {
+  return {
+    container: {
+      height: "100%",
+      color: theme.palette.primary.main,
+      backgroundColor: theme.palette.secondary.main,
+      display: "flex",
+      border: "thin solid",
+      borderColor: theme.palette.primary.main,
+      "&:hover": {
+        outline: "3px solid",
+        outlineColor: theme.palette.primary.main
+      },
+      "&:active": {
+        backgroundColor: theme.palette.primary.main,
+        color: theme.palette.secondary.main,
+        "& img": {
+          filter: "invert(0.75)"
+        }
+      }
+    },
+    title: {
+      fontFamily: "'Archivo', sans-serif",
+      fontSize: "2rem"
+    },
+    introText: {
+      fontFamily: "'Montserrat', sans-serif",
+      fontSize: "1rem",
+      textOverflow: "ellipsis"
+    },
+    metaData: {
+      fontFamily: "'Space Mono', sans-serif",
+      fontSize: "0.625rem"
+    },
+    detailsContainer: {
+      flex: 1,
+      padding: 20
+    },
+    imageContainer: {
+      border: "10px solid",
+      borderLeft: "11px solid",
+      borderColor: theme.palette.primary.main
+    },
+    image: {
+      objectFit: "cover",
+      width: 160,
+      height: "100%"
+    },
+    focusVisble: {
+      outline: "3px solid",
+      outlineColor: theme.palette.primary.main
+    }
+  };
 });
 
 function PostPreview(props) {
   const classes = useStyles();
-  const history = useHistory();
-
-  function goToPost() {
-    history.push(`/journal/${props.post.url}`);
-  }
 
   return (
-    <Container classes={{ root: classes.container }} onClick={goToPost}>
-      <div className={classes.text}>
-        <Typography className={classes.title}>
-          {props.post.intro_title}
-        </Typography>
-        <Typography className={classes.introText}>
-          {props.post.intro_content}
-        </Typography>
+    <ButtonBase
+      component={Link}
+      to={`/journal/${props.post.url}`}
+      disableRipple
+      focusVisibleClassName={classes.focusVisble}
+    >
+      <div className={classes.container}>
+        <div className={classes.detailsContainer}>
+          <Typography className={classes.title}>
+            {props.post.intro_title}
+          </Typography>
+          <Typography className={classes.introText}>
+            {props.post.intro_content}
+          </Typography>
+        </div>
+        {props.post.intro_image ? (
+          <Hidden xsDown>
+            <div className={classes.imageContainer}>
+              <img src={props.post.intro_image} className={classes.image} />
+            </div>
+          </Hidden>
+        ) : null}
       </div>
-      {props.post.intro_image ? (
-        <img src={props.post.intro_image} className={classes.image} />
-      ) : null}
-    </Container>
+    </ButtonBase>
   );
 }
 
