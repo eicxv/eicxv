2741435bf30c7eae7f01e0738978c6965329323d
Einar Persson <einar.jpg@gmail.com>
2021-12-10 13:27:55 +0100

    fixed dynamic relaxation exports

--- packages/dynamic-relaxation/src/index.js
+++ packages/dynamic-relaxation/src/index.js
@@ -1,4 +1,4 @@
 export { default as Canvas } from './components/dynamic-relaxation-factory';
-export { default as cableSystemDemo } from './refactor/demos/cable-system/main';
-export { default as pendulumDemo } from './refactor/demos/pendulum/main';
-export { default as minimalSurfaceDemo } from './refactor/demos/minimal-surface/main';
+export { default as cableSystemDemo } from './demos/cable-system/main';
+export { default as pendulumDemo } from './demos/pendulum/main';
+export { default as minimalSurfaceDemo } from './demos/minimal-surface/main';
